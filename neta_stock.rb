dfb4b1023ad0c54ce0608a52ac6b9626bebaac18
kimihito <tatsurotamashiro@gmail.com>
2013-02-23 17:21:24 +0900

    post stock tweet

--- neta_stock.rb
+++ neta_stock.rb
@@ -2,21 +2,29 @@
 #-*- coding: utf-8 -*-
 require "tweetstream"
 TweetStream.configure do |config|
-  config.consumer_key = ENV["CONSUMER_KEY"]
-  config.consumer_secret = ENV["CONSUMER_SECRET"]
-  config.oauth_token = ENV["OAUTH_TOKEN"]
-  config.oauth_token_secret = ENV["OAUTH_SECRET"]
-  config.auth_method = :oauth
+   config.consumer_key = ENV["CONSUMER_KEY"]
+   config.consumer_secret = ENV["CONSUMER_SECRET"]
+   config.oauth_token = ENV["OAUTH_TOKEN"]
+   config.oauth_token_secret = ENV["OAUTH_SECRET"]
+   config.auth_method = :oauth
+end
+
+Twitter.configure do |config|
+   config.consumer_key = ENV["CONSUMER_KEY"]
+   config.consumer_secret = ENV["CONSUMER_SECRET"]
+   config.oauth_token = ENV["OAUTH_TOKEN"]
+   config.oauth_token_secret = ENV["OAUTH_SECRET"]
+   config.auth_method = :oauth
 end
 
 client = TweetStream::Client.new
 client.track("@geeoki_hack") do |status|
+  puts status.text
   neta = "#{status.text.gsub("@geeoki_hack ","")}\n" 
   puts neta
-  open("neta.txt","a") do |f|
-    f.write(neta)
-    Twitter.update("@#{status.user.screen_name} ストックしました。ありがとう" )
-  end
+  open("neta.txt", "a") {|f| f.write neta}
+  Twitter.update("@#{status.user.screen_name} ストックしました。ありがとう" )
+  puts neta
 end
 
 client.userstream
--- neta_tweet.rb
+++ neta_tweet.rb
@@ -3,12 +3,17 @@
 require 'twitter'
 
 Twitter.configure do |config|
-  config.consumer_key = ENV["CONSUMER_KEY"]
-  config.consumer_secret = ENV["CONSUMER_SECRET"]
-  config.oauth_token = ENV["OAUTH_TOKEN"]
-  config.oauth_token_secret = ENV["OAUTH_SECRET"]
+  config.consumer_key = "kSgU7H98OaQEz3nLF2q2A" 
+  config.consumer_secret = "u9wsFag4aKTY0vXshid2wryTPqeNeoR7LHPiXQCDU"
+  config.oauth_token = "1210405740-IAWVtONKBJ2jawDsYMEgu7CSaf5srVKHPr5Zwz0"
+  config.oauth_token_secret = "tGqq4BmTwV9Urzh1v7mdKqqbtNT6MrfUHkgpspmUa0"
+  # config.consumer_key = ENV['CONSUMER_KEY']
+  # config.consumer_secret = ENV["CONSUMER_SECRET"]
+  # config.oauth_token = ENV["OAUTH_TOKEN"]
+  # config.oauth_token_secret = ENV["OAUTH_SECRET"]
 end 
 
-netas = open("neta.txt").readlines
+netas = open("neta.txt").readlines.sample(2)
 
-Twitter.update(netas.sample(2))
+Twitter.update("#{netas[0]}と #{netas[1]}はいかがでしょう？") 
+
